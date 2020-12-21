749af72cfdd3c192c1e225e8d1fbe904d2ec38a9
Brian Toney <btoney1988@gmail.com>
2020-12-21 16:03:27 -0500

    Working Product

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const mongoose = require("mongoose");
 const htmlRoutes = require("./routes/htmlRoutes");
 const apiRoutes = require("./routes/apiRoutes");
 
-
 const PORT = process.env.PORT || 3000;
 
 let app = express();
@@ -15,11 +14,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-
 htmlRoutes(app);
 apiRoutes(app);
-
-
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
   useNewUrlParser: true,
